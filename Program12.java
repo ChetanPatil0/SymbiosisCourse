877ba279fc028160aff938478d67412b40835ddd
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-10 10:01:09 +0530

    Day 10 Program 1

---
+++ Program12.java
@@ -0,0 +1,27 @@
+//Print the pattern
+// 0 0
+// 0 1
+
+// 1 0
+// 1 1
+
+// 2 0
+// 2 1
+class Program12
+{
+    public static void main(String args[])
+    {
+      
+        for(int i =0; i<3; i++)
+        {
+            
+            for(int j=0;j<=1;j++)
+            {
+            
+                    System.out.print(i + "\t"+ j+"\n");
+                }
+                
+            }
+                System.out.print("\n");
+        }
+    }
