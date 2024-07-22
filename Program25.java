7d9a2ff18e2d4a67d4e6ae619662555a1b962d2a
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-22 10:20:15 +0530

    Add Program 25 file

---
+++ Program25.java
@@ -0,0 +1,12 @@
+public class Program25{
+    static int i=10;
+    public static void main(String args[])
+    {
+        changevalue(i);
+        System.out.println(i);
+    }
+    public static void changevalue(int i)
+    {
+        i=50;
+    }
+}
