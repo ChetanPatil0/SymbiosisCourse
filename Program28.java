4405dab82d0929645cccd380f8c9fdf5dc3520b4
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-25 21:23:23 +0530

    add Program28 File

---
+++ Program28.java
@@ -0,0 +1,11 @@
+public class Program28{
+    public static void main(String args[])
+    {
+        System.out.println(fun());
+    }
+    public static void fun()
+    {
+        int i=0;
+        return ++i;
+    }
+}
