f836b42cde48ac778feef347bb3b14b6591e8cc9
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-30 10:14:11 +0530

    add Program30 File

---
+++ Program30.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Program30{
+    public static void main(String args[])
+    {
+   Scanner Sc = new Scanner(System.in);
+   String S1 = "hello";
+   
+   String S2 = "hello";
+   String S3 = new String("hello");
+   System.out.println(S1 == S2);
+   System.out.println(S2.equals(S3));
+
+   Static int a[];
+   System.out.println(a.length);
+   }
+}
