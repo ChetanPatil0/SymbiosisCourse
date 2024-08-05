df1539d436a667e7295f435cc43c2d32495ede98
chetanpatil0 <patilchetan2905@gmail.com>
2024-08-05 08:42:40 +0530

    add program36 file

---
+++ Program36.java
@@ -0,0 +1,11 @@
+//JAVA Pattern
+class Program36{
+    public static void main(String args[])
+    {
+        System.out.println("    J   a  v       v  a");
+        System.out.println("    J  a a  v     v  a a");
+        System.out.println("J   J a a a  v   v  a a a");
+        System.out.println("  J  a     a   v   a     a");
+        
+    }
+}
