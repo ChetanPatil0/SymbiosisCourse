dbcb86175114c371faa5a6bff131665a2baf3fe5
chetanpatil0 <patilchetan2905@gmail.com>
2024-08-05 08:42:53 +0530

    add program37 file

---
+++ Program37.java
@@ -0,0 +1,14 @@
+//
+import java.util.*;
+class Program37{
+    public static void main(String args[])
+    {
+            TreeSet ts = new TreeSet();
+
+            ts.add(1);
+            ts.add(2);
+            ts.add(3);
+            System.out.println(ts);
+
+    }
+}
